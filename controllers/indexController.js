7daa8c7b0efd43ab2ada210a25c1ac98c6e25fa5
TerrenceP415 <padillaterrence@gmail.com>
2026-07-16 22:47:54 +0800

    Fixed Suggested Flights in Index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,12 +2,13 @@ const Flight = require('../models/flight');
 const Reservation = require('../models/reservation');
 
 exports.getStatistics = async () => {
+    //get statistics
     const [availableFlights, activeBookings, destinations] = await Promise.all([
         Flight.countDocuments({ availableSeats: { $gt: 0 } }),
         Reservation.countDocuments({ status: { $ne: 'Cancelled' } }),
         Flight.distinct('destination').then((flightDestinations) => flightDestinations.length),
     ]);
-
+    //return statistics as these variables
     return {
         availableFlights,
         activeBookings,
@@ -17,12 +18,17 @@ exports.getStatistics = async () => {
 
 exports.getIndexPage = async (req, res) => {
     try {
-        const stats = await exports.getStatistics();
-
+        const [stats, latestFlight] = await Promise.all([
+            exports.getStatistics(),
+            //find the latest flight created for suggested flight
+            Flight.findOne().sort({ createdAt: -1 }).lean(),
+        ]);
+        //send over stats and latest flight
         res.render('index', {
             title: 'Home',
             layout: 'main',
             ...stats,
+            latestFlight,
         });
     } catch (error) {
         console.error('Error loading homepage statistics:', error);
