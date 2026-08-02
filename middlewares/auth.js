93c0da2cf090fce10262a6adaa733f3232cfc250
TerrenceP415 <padillaterrence@gmail.com>
2026-08-02 10:21:06 +0800

    Added requireRole logic

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,6 +8,17 @@ const isAuthenticated = (req, res, next) => {
   return res.redirect('/login');
 };
 
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.session && req.session.user && req.session.user.role === role) {
+      return next();
+    }
+    
+    return res.redirect('/');
+  };
+}
+
 module.exports = {
   isAuthenticated,
+  requireRole,
 };
